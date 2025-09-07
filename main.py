17e93deae3f746b18c51f94732cef1cf5f05ebc8
Senne Hollard <sennehollard@sennes-air.home>
2025-09-07 17:37:22 +0200

    Complete project setup and add numpy script

---
+++ main.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+
+def add_two_integer_lists(a: list[int], b: list[int]) -> list[int]:
+    """Add two lists of integers element-wise."""
+    arr = np.array(a) + np.array(b)
+    print(f"My added array: {arr}")
+    return arr.tolist()
+
+
+if __name__ == "__main__":
+    add_two_integer_lists([1, 2, 3], [4, 5, 6])
